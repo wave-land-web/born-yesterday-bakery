d63400f8b159115899ac8178b17636e9cae2ccaf
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-07-10 15:54:24 -0500

    initial sourcing page boilerplate + setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bornyesterdaybakery.com',
+  scopedStyleStrategy: 'class',
   prefetch: {
     prefetchAll: true,
   },
